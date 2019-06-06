4759b4bf769fe9027cfb2b9d811c9cd6cbabb51c
minoro75 <minoro75@gmail.com>
2019-06-07 00:24:27 +0300

    Initial commit

---
+++ app/src/main/java/ru/minoro75/proj/kappatesting/MainActivity.kt
@@ -0,0 +1,44 @@
+package ru.minoro75.proj.kappatesting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+
+/*attach required Fragments to ViewPager and Viewpager to TabLayout*/
+class MainActivity : AppCompatActivity() {
+
+    lateinit var viewPager:ViewPager
+    lateinit var tabLayout:TabLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //TODO: USE ANKO HERE
+        viewPager = findViewById(R.id.pager)
+        tabLayout = findViewById(R.id.tabs)
+
+        tabLayout.addTab(tabLayout.newTab().setText("Most Emailed"))
+        tabLayout.addTab(tabLayout.newTab().setText("Most Shared"))
+        tabLayout.addTab(tabLayout.newTab().setText("Most Viewed"))
+
+        tabLayout.tabGravity=TabLayout.GRAVITY_FILL
+        //TODO:ADAPTER FOR TABS
+        val adapter = TabAdapter(this,supportFragmentManager,tabLayout.tabCount)
+        viewPager.adapter =adapter
+
+        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabReselected(p0: TabLayout.Tab?) {
+            }
+
+            override fun onTabUnselected(p0: TabLayout.Tab?) {
+            }
+
+            override fun onTabSelected(p0: TabLayout.Tab?) {
+                viewPager!!.currentItem = p0!!.position
+            }
+        })
+    }
+}
---
+++ app/src/main/java/ru/minoro75/proj/kappatesting/TabAdapter.kt
@@ -0,0 +1,25 @@
+package ru.minoro75.proj.kappatesting
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import ru.minoro75.proj.kappatesting.fragments.EmailedFragment
+import ru.minoro75.proj.kappatesting.fragments.SharedFragment
+import ru.minoro75.proj.kappatesting.fragments.ViewedFragment
+
+class TabAdapter(private val myContext: Context, fm:FragmentManager, internal var totalTabs:Int) : FragmentPagerAdapter(fm){
+    override fun getItem(position: Int): Fragment? {
+    when(position){
+        0-> return EmailedFragment()
+        1-> return SharedFragment()
+        2-> return ViewedFragment()
+        else-> return null
+    }
+    }
+
+    override fun getCount(): Int {
+    return totalTabs
+    }
+
+}
